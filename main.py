e88406c749e7739fa318e844dafa139b0c660c54
traditionallist555-glitch <traditionallist555@gmail.com>
2026-07-30 15:32:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHANNEL_ID = os.environ.get("TELEGRAM_CHANNEL_ID") or os.environ.get("TELEGRAM_CHAT_ID")
 ALPHA_VANTAGE_KEY = os.environ.get("ALPHA_VANTAGE_KEY", "demo")
 
-TRIGGER_HOUR = 6
-TRIGGER_MINUTE = 0
+TRIGGER_HOUR = 14
+TRIGGER_MINUTE = 35
 
 daily_ledger = {
     "date": None,
