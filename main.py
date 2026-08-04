4b33c919ff0a2c3a3be9292ae9391d7ca6041d2d
traditionallist555-glitch <traditionallist555@gmail.com>
2026-08-04 10:35:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -57,9 +57,9 @@ TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHANNEL_ID = os.environ.get("TELEGRAM_CHANNEL_ID") or os.environ.get("TELEGRAM_CHAT_ID")
 ALPHA_VANTAGE_KEY = os.environ.get("ALPHA_VANTAGE_KEY", "demo")
 
-# Updated to 8:00 AM trigger time as requested
-TRIGGER_HOUR = 8
-TRIGGER_MINUTE = 58
+# Updated to 9:40 AM trigger time as requested
+TRIGGER_HOUR = 9
+TRIGGER_MINUTE = 40
 
 # --- 🎯 DIRECT MT5 BROKER PRICE OVERRIDE ---
 LIVE_MT5_PRICE_OVERRIDE = 4032.39 
